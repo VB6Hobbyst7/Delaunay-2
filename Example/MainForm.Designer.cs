e2d462d45e4eaf659f729992d606e0eaa375d80b
Nathan Garside <work@nathangarside.com>
2013-12-30 08:47:48 +0000

    Added example project

---
+++ Example/MainForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Example
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.preview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // preview
+            // 
+            this.preview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.preview.Location = new System.Drawing.Point(0, 0);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(584, 361);
+            this.preview.TabIndex = 0;
+            this.preview.TabStop = false;
+            this.preview.Paint += new System.Windows.Forms.PaintEventHandler(this.preview_Paint);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.preview);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Example";
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox preview;
+    }
+}
+
---
+++ Example/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using Delaunay;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Example
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void preview_Paint(object sender, PaintEventArgs e)
+        {
+            List<PointF> points = GetRandomPoints();
+            RectangleF size = new RectangleF(0, 0, preview.Width, preview.Height);
+            Voronoi voronoi = new Voronoi(points, null, size);
+            foreach (PointF point in points)
+            {
+                List<PointF> region = voronoi.Region(point);
+                e.Graphics.DrawRectangle(Pens.Red, point.X, point.Y, 10, 10);
+                e.Graphics.DrawLines(Pens.Black, region.ToArray());
+            }
+        }
+
+        private List<PointF> GetRandomPoints()
+        {
+            List<PointF> points = new List<PointF>();
+            Random random = new Random();
+            int border = 10;
+            for (int i = 0; i < 20; i++)
+            {
+                float x = random.Next(border, preview.Width - border * 2);
+                float y = random.Next(border, preview.Height - border * 2);
+                points.Add(new PointF(x, y));
+            }
+            return points;
+        }
+    }
+}
